b4ae16f4a7b3d3ea8725ddaef52680b211a946c9
Riyaz <riyazpasha880@gmail.com>
2021-07-12 22:50:16 +0530

    Moved calucluate charge method to the rental class

--- src/example1/Customer.java
+++ src/example1/Customer.java
@@ -28,7 +28,7 @@ public class Customer {
             Double thisAmount = Double.valueOf(0);
             Rental eachRental = (Rental) _rentals.nextElement();
 
-            thisAmount = amountFor(eachRental);
+            thisAmount = eachRental.getCharge();
 
             frequentRenterPoints++;
 
@@ -40,24 +40,6 @@ public class Customer {
 
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
-    }
-
-    private Double amountFor(Rental aRental) {
-        Double result = Double.valueOf(0);
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2) result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-        }
         return result;
     }
 }
--- src/example1/Rental.java
+++ src/example1/Rental.java
@@ -16,4 +16,22 @@ public class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+    public Double getCharge() {
+        Double result = Double.valueOf(0);
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (this.getDaysRented() > 2) result += (this.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (this.getDaysRented() > 3) result += (this.getDaysRented() - 2) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
