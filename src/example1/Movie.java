fe2ac69b32fdaec9fc18a47d49e6d22b801cf676
Riyaz <riyazpasha880@gmail.com>
2021-07-13 22:01:54 +0530

    Move get charge method to the Price class

--- src/example1/Movie.java
+++ src/example1/Movie.java
@@ -7,7 +7,7 @@ public class Movie {
     public static final int CHILDREN = 2;
 
     private String title;
-    private Price priceCode;
+    Price priceCode;
 
     public Movie(String title, int priceCode) {
         this.title = title;
@@ -43,22 +43,9 @@ public class Movie {
     }
 
     public Double getCharge(int daysRented) {
-        Double result = Double.valueOf(0);
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3) result += (daysRented - 2) * 1.5;
-                break;
-        }
-        return result;
+        return this.priceCode.getCharge(daysRented);
     }
+
 
     public int getFrequentRenterPoints(int daysRented) {
         return (this.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
--- src/example1/Price.java
+++ src/example1/Price.java
@@ -2,4 +2,22 @@ package example1;
 
 public abstract class Price {
     abstract int getPriceCode();
+
+    public Double getCharge(int daysRented) {
+        Double result = Double.valueOf(0);
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) result += (daysRented - 2) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
