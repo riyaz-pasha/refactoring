39a217f7f7362113e19d9e18d24d3893ea96e237
Riyaz <riyazpasha880@gmail.com>
2021-07-13 21:43:16 +0530

    Move methods to the appropriate class. Single responsibility

--- src/example1/Movie.java
+++ src/example1/Movie.java
@@ -30,6 +30,30 @@ public class Movie {
         this.priceCode = priceCode;
     }
 
+    public Double getCharge(int daysRented) {
+        Double result = Double.valueOf(0);
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) result += (daysRented - 2) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return (this.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+                ? 2
+                : 1;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
--- src/example1/Rental.java
+++ src/example1/Rental.java
@@ -18,27 +18,13 @@ public class Rental {
     }
 
     public Double getCharge() {
-        Double result = Double.valueOf(0);
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (this.getDaysRented() > 2) result += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (this.getDaysRented() > 3) result += (this.getDaysRented() - 2) * 1.5;
-                break;
-        }
-        return result;
+        return this.getMovie()
+                .getCharge(this.getDaysRented());
     }
 
     public int getFrequentRenterPoints() {
-        if (this.getMovie().getPriceCode() == Movie.NEW_RELEASE && this.getDaysRented() > 1)
-            return 2;
-        return 1;
+        return this.getMovie()
+                .getFrequentRenterPoints(this.getDaysRented());
     }
 
 }
